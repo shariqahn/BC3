46ddf844a14ac90f46a7bc1dfb9e714bd08d1832
Shariqah Hossain <shossain@mit.edu>
2024-04-24 11:18:17 -0400

    updated pattern scaling and temp values

--- api/main.py
+++ api/main.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, request
 import netCDF4 as nc
 import numpy as np
-import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
 
 from flask_cors import CORS
+
+# ################ OLD PS #####################
+# def patternscaling( Tbar, filename ):
+#     # Tbar can be either absolute temperature or anomaly
+#     # The function checks if Tbar is lower than 100 to determine which one is which
+#     n4obj      = nc.Dataset( filename, 'r' )
+#     lon        = n4obj.variables['lon']
+#     lat        = n4obj.variables['lat']
+#     alpha      = n4obj.variables['slope']
+#     beta       = n4obj.variables['intercept']
+#     if Tbar < 100:
+#         # User likely passed temperature anomaly
+#         pstemp = alpha[:]*Tbar # Temperature Anomaly [K]
+#     else: 
+#         # User likely passed absolute temperatre
+#         pstemp = alpha[:]*Tbar + beta[:] # Absolute Temperature [K]
+#     # Return 
+#     return lat[:], lon[:], pstemp[:]
+# #####################################
 
 #####################################
 def patternscaling( Tbar, filename ):
@@ -14,15 +31,15 @@ def patternscaling( Tbar, filename ):
     lon        = n4obj.variables['lon']
     lat        = n4obj.variables['lat']
     alpha      = n4obj.variables['slope']
-    beta       = n4obj.variables['intercept']
-    if Tbar < 100:
-        # User likely passed temperature anomaly
-        pstemp = alpha[:]*Tbar # Temperature Anomaly [K]
-    else: 
-        # User likely passed absolute temperatre
-        pstemp = alpha[:]*Tbar + beta[:] # Absolute Temperature [K]
+    # preindustrial climatology
+    # i.e. on average what the temperature (or precipitation) was everywhere in preindustrial conditions
+    climo      = n4obj.variables['climatology']
+    pspred     = alpha[:]*Tbar # Anomaly
+    # relative change in percentage compared to climatology 
+    # (e.g. value of 50% means that in that location PS predicts that it would rain 50% more than the preindustrial average)    
+    relchange  = pspred/climo * 100
     # Return 
-    return lat[:], lon[:], pstemp[:]
+    return lat[:], lon[:], pspred[:], relchange, climo[:]
 #####################################
 
 app = Flask(__name__)
@@ -31,9 +48,9 @@ CORS(app)
 @app.route('/temperature')
 def get_temperature():
     # try: 
-    filename = 'PatternScalingCoefficients_ssp370r1i1p1f1-ssp585r1i1p1f1_Regridded.nc'
+    filename = 'PatternScalingCoefficients_tas_ssp245-ssp370__Spatial_Regridded.nc'
     tbar              = float(request.args.get('tbar', 0))
-    flat, flon, ftemp = patternscaling( tbar, filename )
+    flat, flon, ftemp, _, _ = patternscaling( tbar, filename )
     
     return {
         "lats": flat.tolist(),
@@ -41,6 +58,26 @@ def get_temperature():
         "temps": ftemp.tolist(),
         "minTemperature": str(np.min(ftemp)), 
         "maxTemperature": str(np.max(ftemp))
+    }
+    # except:
+    #     return 'Please enter valid numbers in the URL.'
+
+@app.route('/precipitation')
+def get_precipitation():
+    # try: 
+    filename = 'PatternScalingCoefficients_pr_ssp245-ssp370__Spatial_Regridded.nc'
+    tbar              = float(request.args.get('tbar', 0))
+    flat, flon, fprecipitation, fprel, _  = patternscaling( tbar, filename )
+    print('lat', flat)
+    print('lon', flat)
+    print('len', len(flat), len(flon))
+    
+    return {
+        "lats": flat.tolist(),
+        "lons": flon.tolist(),
+        "change": fprel.tolist(),
+        # "minPrecipitation": str(np.min(fprecipitation)), 
+        # "maxPrecipitation": str(np.max(fprecipitation))
     }
     # except:
     #     return 'Please enter valid numbers in the URL.'
