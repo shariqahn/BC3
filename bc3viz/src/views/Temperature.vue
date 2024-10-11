44082c4e82600c5f231ec0e359372bc06e88d50b
Shariqah Hossain <shossain@mit.edu>
2024-10-11 10:59:48 -0400

    prod url

--- bc3viz/src/views/Temperature.vue
+++ bc3viz/src/views/Temperature.vue
@@ -248,11 +248,11 @@ export default {
     },
     async getTemperatureData() {
       let url = window.location.origin
-      // const path = url + '/api/temperature?tbar=' + this.tbar + '&resolution=' + this.resolution;
+      const path = url + '/api/temperature?tbar=' + this.tbar + '&resolution=' + this.resolution;
 
       // for local testing
-      url = url.slice(0, url.lastIndexOf(":"))
-      const path = url + ':5002/temperature?tbar=' + this.tbar + '&resolution=' + this.resolution;
+      // url = url.slice(0, url.lastIndexOf(":"))
+      // const path = url + ':5002/temperature?tbar=' + this.tbar + '&resolution=' + this.resolution;
 
       try {
         const response = await axios.get(path);
