75d9b6bcbe1bddad85473b7e9718bfaab864eda7
Shariqah Hossain <shossain@mit.edu>
2025-01-16 14:09:46 -0500

    format zoom buttons for en-roads

--- bc3viz/test.js
+++
@@ -1,8 +0,0 @@
-handleTooltipChanged(event, tooltip) {
-  const iframeType = "bc3-map-iframe-current" === event.id ? "current" : "baseline";
-  console.log(`==> En-ROADS received "tooltip-changed" message from BC3 "${iframeType}" map iframe: ${JSON.stringify(tooltip)}`);
-  sd(
-    "current" === iframeType ? this.baselineMapIFrame : this.currentScenarioMapIFrame,
-    { kind: "set-tooltip", lat: tooltip == null ? void 0 : tooltip.lat, lon: tooltip == null ? void 0 : tooltip.lon }
-  );
-}
